d2678ec47305b3ed92dcf9de341ac6ca12af0840
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-10-08 22:41:59 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan34/objectorientedkalkulator/Kalkulator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan34.objectorientedkalkulator;
+
+/**
+ *
+ * @author 
+ * NAMA         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * KELAS        : PBO3
+ */
+public class Kalkulator {
+     public double value1;
+    public double value2;
+
+    public double tambahBilangan(){ return value1+value2;}
+    public double kurangBiangan(){ return value1-value2;}
+    public double kaliBilangan(){ return value1*value2;}
+    public double bagiBilangan(){ return value1/value2;}
+    public double sisaBilangan(){ return value1%value2;}
+}
---
+++ src/pbo3/pkg10117096/latihan34/objectorientedkalkulator/PBO310117096Latihan34ObjectOrientedKalkulator.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan34.objectorientedkalkulator;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 
+ * NAMA         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * KELAS        : PBO3
+ * DESKRIPSI    : Program ini berisi program kalkulator yang datanya diinput
+ * oleh user
+ */
+public class PBO310117096Latihan34ObjectOrientedKalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner scn = new Scanner(System.in);
+        Kalkulator kal = new Kalkulator();
+
+        System.out.println("====Aplikasi Kalkulator Bilangan====");
+        System.out.print("Masukkan Angka ke-1 : ");
+        kal.value1 = scn.nextDouble();
+        System.out.print("Masukkan Angka ke-2 : ");
+        kal.value2 = scn.nextDouble();
+
+        //format dua digit di belakang koma
+        DecimalFormat df = new DecimalFormat("###.##");
+
+        System.out.println("\nHasil Penjumlahan : "+kal.tambahBilangan());
+        System.out.println("Hasil Pengurangan : "+kal.kurangBiangan());
+        System.out.println("Hasil Perkalian : "+kal.kaliBilangan());
+        System.out.println("Hasil Pembagian : "+df.format(kal.bagiBilangan()));
+        System.out.println("Hasil Sisa : "+kal.sisaBilangan());
+    }
+    
+}
